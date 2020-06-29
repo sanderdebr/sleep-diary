8a8f87e617d37a7f4a72217607c2373af1a5515f
sanderdebr <sander606@gmail.com>
2020-06-29 11:43:47 +0200

    ready auth deploy

--- src/common/credentials.js
+++ src/common/credentials.js
@@ -2,5 +2,8 @@ require("dotenv").config();
 
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
-export const REDIRECT_URIS = "http://localhost:8000/auth/sign-in-callback";
+export const REDIRECT_URIS =
+  process.env.NODE_ENV === "production"
+    ? "https://sleepdiary.nl/auth/sign-in-callback"
+    : "http://localhost:8000/auth/sign-in-callback";
 export const JWT_SECRET = process.env.JWT_SECRET;
