2575ab3a06dcbe2720ad3fda2c33ad2e8329c57c
sanderdebr <sander606@gmail.com>
2020-12-08 12:56:14 +0100

    changes for deployment

--- src/common/credentials.js
+++ src/common/credentials.js
@@ -4,7 +4,7 @@ export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
 export const REDIRECT_URIS =
   process.env.NODE_ENV === "production"
-    ? "http://sleepdiary.sanderdev.nl/auth/sign-in-callback"
+    ? "https://sleepdiary.sanderdev.nl/auth/sign-in-callback"
     : "http://localhost:8000/auth/sign-in-callback";
 export const JWT_SECRET = process.env.JWT_SECRET;
 
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,11 +3,9 @@ import * as Session from "~/src/common/session";
 export default async (req, res, app) => {
   const { session } = await Session.getSession(req);
 
-  console.log("SESSIOn", session);
-
-  // if (!session || session.error) {
-  //   return app.render(req, res, "/auth/sign-in-error/", { session: null });
-  // }
+  if (!session || session.error) {
+    return app.render(req, res, "/auth/sign-in-error/", { session: null });
+  }
 
   return app.render(req, res, "/dashboard", { session });
 };
