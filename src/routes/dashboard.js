5e672792e12008cbbfe8ec8be9991a4c8ab73c96
sanderdebr <sander606@gmail.com>
2020-12-08 12:36:55 +0100

    changes for deployment

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,9 +3,11 @@ import * as Session from "~/src/common/session";
 export default async (req, res, app) => {
   const { session } = await Session.getSession(req);
 
-  if (!session || session.error) {
-    return app.render(req, res, "/auth/sign-in-error/", { session: null });
-  }
+  console.log("SESSIOn", session);
+
+  // if (!session || session.error) {
+  //   return app.render(req, res, "/auth/sign-in-error/", { session: null });
+  // }
 
   return app.render(req, res, "/dashboard", { session });
 };
