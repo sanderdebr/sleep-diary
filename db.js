d390f1e129412db5997116093818d5423e9117c8
sanderdebr <sander606@gmail.com>
2020-12-08 13:03:04 +0100

    changes for deployment

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 
 import configs from "~/knexfile";
 import knex from "knex";
 
-const environment =
-  process.env.NODE_ENV !== "production" ? "development" : "production";
-const envConfig = configs[environment];
+const envConfig = configs[process.env.NODE_ENV];
 const db = knex(envConfig);
+
+console.log(envConfig, db);
 
 module.exports = db;
